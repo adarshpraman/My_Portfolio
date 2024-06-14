39cdc9d7e69793840a925126d3103d9270251e44
Adarsh <adarshpraman@gmail.com>
2024-06-14 18:21:47 +0530

    updated resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -58,8 +58,7 @@ const Home = () => {
             </Link>
 
             <a
-              href="/path/to/resume.pdf"
-              download="Adarsh_Praman_Resume.pdf"
+              href="https://drive.google.com/file/d/1QWMO2wJeG10_ksPyk8YMxDPCK9Dk0uwq/view?usp=sharing"
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 transition-transform duration-300"
             >
               Download CV
